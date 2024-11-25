d748df3088f6ab27ef1c9ab94d86ab068219388a
Poetazy <129734555+thihuynh219@users.noreply.github.com>
2024-11-25 20:30:06 +0700

    sửa thành menu QL_SP

--- QL_SP.java
+++ QL_SP.java
@@ -1,51 +1,55 @@
 import java.util.Scanner;
 
 public class QL_SP {
-    public static void main(String[] args) {
+    
+    public void menu() {
+       
         Scanner scanner = new Scanner(System.in);
         DS_SP quanLy = new DS_SP();
-
-        // Đọc dữ liệu từ file khi khởi động chương trình
-        quanLy.docFile();
+        quanLy.docFile();// Đọc dữ liệu từ file khi khởi động chương trình
 
         String chonChucNang;
 
         do {
             // Hiển thị menu lựa chọn
             System.out.println("========== QUAN LY SAN PHAM ==========");
-            System.out.println("1. Them san pham");
-            System.out.println("2. Sua san pham");
-            System.out.println("3. Xoa san pham");
-            System.out.println("4. Tim kiem san pham");
-            System.out.println("5. Xem danh sach san pham");
+            System.out.println("1. Nhap danh sach san pham");
+            System.out.println("2. Xuat danh sach san pham");
+            System.out.println("3. Them san pham");
+            System.out.println("4. Sua san pham");
+            System.out.println("5. Xoa san pham");
+            System.out.println("6. Tim kiem san pham");
             System.out.println("0. Thoat");
             System.out.print("Chon chuc nang: ");
             chonChucNang = scanner.nextLine(); // Đọc lựa chọn từ người dùng
 
             switch (chonChucNang) {
                 case "1":
-                        quanLy.nhap();// Thêm sản phẩm
+                        quanLy.nhap();// Nhập danh sách sản phẩm
                     break;
 
                 case "2":
-                        quanLy.sua();// Sửa sản phẩm
+                        quanLy.xuat();// Xem danh sách sản phẩm
                     break;
 
                 case "3":
-                        quanLy.xoa(); // Xóa sản phẩm
+                        quanLy.them(); //Thêm mới sản phẩm
                     break;
 
                 case "4":
-                        quanLy.timKiem();// Tìm sản phẩm
+                        quanLy.sua();   // Sửa sản phẩm
                     break;
 
                 case "5":
-                    quanLy.xuat(); // Xem danh sách sản phẩm
+                    quanLy.xoa(); // Xóa sản phẩm
+                    break;
+                    
+                case "6":
+                    quanLy.timKiem();// Tìm sản phẩm
                     break;
 
-
                 case "0":
-                    System.out.println("Thoat chuong trinh.");
+                    System.out.println("Thoat.");
                     break;
 
                 default:
